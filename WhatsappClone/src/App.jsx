3ec3241eab84108c0ba26250fbe48053c6292cdf
Abhijith <jithumitnick@gmail.com>
2025-04-09 10:51:33 +0530

    Adde icons and made the interface look good

--- WhatsappClone/src/App.jsx
+++ WhatsappClone/src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css'
-
+import SideMenu from './components/sideMenu'
 function App() {
 
 
   return (
     <>
-      <h1 className='text-white 2xl'>Hello </h1>
+      <div className='w-full h-full'>
+        <h1 className='text-zinc-600 2xl'>Hello </h1>
+        <SideMenu />
+      </div>
     </>
   )
 }
---
+++ WhatsappClone/src/components/SideMenu.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import 'remixicon/fonts/remixicon.css'
+function SideMenu() {
+  return (  
+    <div className='flex flex-col gap-5'>
+        <i class="ri-chat-3-line"></i>
+        <i class="ri-cellphone-line"></i>
+        <i class="ri-signal-tower-line"></i>
+        <hr/>
+    </div>
+  )
+}
+
+export default SideMenu
+
+
